de32029eab2007ec634ebbff0164204f12d4b46b
jamesfwz <james@futureworkz.com>
2015-11-14 14:17:38 +0700

    update schema

--- db/migrate/20151114070253_create_open_text_answers.rb
+++ db/migrate/20151114070253_create_open_text_answers.rb
@@ -2,7 +2,7 @@ class CreateOpenTextAnswers < ActiveRecord::Migration
   def change
     create_table :open_text_answers do |t|
       t.text :message
-      t.references :open_text_question, index: true
+      t.references :question, index: true
     end
   end
 end
