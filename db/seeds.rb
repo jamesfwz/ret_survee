b24701dd74a5de5eba3f83be7c8f1eae0021358a
jamesfwz <james@futureworkz.com>
2015-11-15 13:26:04 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
-OpenTextQuestion.first_or_create(title: 'Question 1', content: 'What do you think about Ruby?')
-MultiChoiceQuestion.first_or_create(title: 'Question 2', content: 'What other says:')
-ScaleQuestion.first_or_create(title: 'Question 3', content: 'On a scale of 0 to 100, how much time you spend on Javascript?')
+OpenTextQuestion.find_or_create_by!(title: 'Question 1', content: 'What do you think about Ruby?')
+MultiChoiceQuestion.find_or_create_by!(title: 'Question 2', content: 'What other says:')
+ScaleQuestion.find_or_create_by!(title: 'Question 3', content: 'On a scale of 0 to 100, how much time you spend on Javascript?')
+
+[ "Ruby", "Python", "PHP", "Java" ].each do |skill|
+  Choice.find_or_create_by!(title: skill)
+end
