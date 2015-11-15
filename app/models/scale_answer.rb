3a467317d977fe63a99de3376f5a4af251433fcb
jamesfwz <james@futureworkz.com>
2015-11-15 13:15:37 +0700

    validate value range for ScaleAnswer

--- app/models/scale_answer.rb
+++ app/models/scale_answer.rb
@@ -2,5 +2,5 @@ class ScaleAnswer < ActiveRecord::Base
   belongs_to :question, class_name: ScaleQuestion
 
   validates :question_id, presence: true
-  validates :value,       presence: true
+  validates :value,       presence: true, inclusion: { in: 0..100, message: "The value must be between 0 and 100" }
 end
--- spec/models/scale_answer_spec.rb
+++ spec/models/scale_answer_spec.rb
@@ -4,6 +4,7 @@ describe ScaleAnswer do
   context 'validations' do 
     it { is_expected.to validate_presence_of :question_id }
     it { is_expected.to validate_presence_of :value }
+    it { is_expected.to validate_inclusion_of(:value).in_range(0..100).with_message("The value must be between 0 and 100") }
   end
 
   context 'associations' do 
