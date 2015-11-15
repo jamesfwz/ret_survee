987f64462285152e8fb7a831bb04dbdf85ed145b
jamesfwz <james@futureworkz.com>
2015-11-15 11:58:30 +0700

    add validation for OpenTextAnswer

--- app/models/open_text_answer.rb
+++ app/models/open_text_answer.rb
@@ -1,3 +1,5 @@
 class OpenTextAnswer < ActiveRecord::Base
   belongs_to :question, class_name: OpenTextQuestion
+
+  validates :message, presence: true
 end
--- spec/models/open_text_answer_spec.rb
+++ spec/models/open_text_answer_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe OpenTextAnswer do 
+  context 'validations' do 
+    it { is_expected.to validate_presence_of :message }
+  end
+
   context 'associations' do 
     it { is_expected.to belong_to :question }
   end
